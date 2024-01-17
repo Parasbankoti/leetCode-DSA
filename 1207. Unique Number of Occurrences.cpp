4688bd67e2a33f38b4bd8d9ce64f9f8bef4ab099
paras bankoti <43200580+Parasbankoti@users.noreply.github.com>
2024-01-17 22:44:14 +0530

    Create 1207. Unique Number of Occurrences.cpp

---
+++ 1207. Unique Number of Occurrences.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    bool uniqueOccurrences(vector<int>& arr) {
+        unordered_map<int,int>mp;
+        unordered_set<int>st;
+        for(int i:arr)
+        mp[i]++;
+
+        for(auto i:mp){
+            if(st.count(i.second))
+            return false;
+            st.insert(i.second);
+        }
+        return true;
+    }
+};
