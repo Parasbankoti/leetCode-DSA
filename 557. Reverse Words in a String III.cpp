b2a3a0731285fa1f1c4e6fcb0af26380c3a78c38
paras bankoti <43200580+Parasbankoti@users.noreply.github.com>
2024-02-23 23:35:58 +0530

    Create 557. Reverse Words in a String III.cpp

---
+++ 557. Reverse Words in a String III.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    string reverseWords(string s) {
+        string res="",temp="";
+        for(int i=0;i<s.size();i++){
+            if(s[i] != ' '){
+                temp+=s[i];
+            }
+            else{
+                reverse(temp.begin(),temp.end());
+                res+=temp+ " ";
+                temp="";
+            }
+        }
+        reverse(temp.begin(),temp.end());
+        res+=temp;
+        return res;
+    }
+};
